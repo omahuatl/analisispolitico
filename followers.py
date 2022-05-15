b13bb58d23b3c6f67a3affe4cdfafb0a770f0f29
Oscar Mejia <oscar.ahuatl@gmail.com>
2022-05-14 20:07:43 -0500

    retrieve_followed has logic to support the restrictions of tweepy about number of request every 15 minutes

--- followers.py
+++ followers.py
@@ -58,25 +58,56 @@ def retrieve_followers(twitter_user: str, pagination: str):
         # print(followers_list)
 
 
-def retrieve_followed(twitter_user: str, retrieve_status: bool):
+def retrieve_followed(twitter_user: str, pagination: str):
     # getting the followers and add them into a file to use them later
-    followed_list = []
+    followers_list = []
     bearer_token = os.environ.get("twitter_bearer")
     client = tweepy.Client(bearer_token)
+    the_user = get_user_id(twitter_user)
 
-    with open("follows.txt", mode="w") as file:
-        for response in tweepy.Paginator(client.get_users_following, get_user_id(twitter_user),
-                                         user_fields=["profile_image_url", "id"]):
-            for follows in response.data:
-                followed_list.append(follows)
-                file.write("id=" + str(follows.id) + ", username=" + str(follows.username) + "\n")
-    file.close()
-    #print(len(followed_list))
-    #print(followed_list)
+    try:
+        if pagination == "":
+            the_mode = "w"
+        else:
+            the_mode = "a"
+
+        with open("followed.txt", mode=the_mode) as file:
+            while pagination != "End":
+                if pagination == "":
+                    response = client.get_users_following(the_user, max_results=100,user_fields=["profile_image_url", "id"])
+                else:
+                    response = client.get_users_following(the_user, max_results=100,pagination_token=pagination,user_fields=["profile_image_url", "id"])
+                print(response.meta)
+
+                lista = list(response.meta.keys())
+                if lista.count('next_token')>0:
+                    pagination = response.meta["next_token"]
+                else:
+                    pagination="End"
+
+                for followed in response.data:
+                    followers_list.append(followed)
+                    file.write("id=" + str(followed.id) + ", username=" + str(followed.username) + "\n")
+
+    except (NameError, TypeError) as error:
+        print(f"General error..: {error}")
+    except tweepy.errors.TooManyRequests as error_message:
+        print(f"tweepy error: {error_message} ")
+    except file.errors as error_message:
+        print(f"file error: {error_message} closing followed file")
+    else:
+        print(f"the followed file was created successfully")
+    finally:
+        file.close()
+        return pagination
+        # print(len(followers_list))
+        # print(followers_list)
 
 
 if __name__ == '__main__':
-    print("retrieve users...")
+
+    """
+    print("retrieve followers...")
     more_users = retrieve_followers("CarlosRangelM78","")
 
     while more_users != "End":
@@ -86,4 +117,15 @@ if __name__ == '__main__':
         now = datetime.now()
         print(f'Continuing retrieving  {now.strftime("%H:%M:%S")}')
         more_users = retrieve_followers("CarlosRangelM78", more_users)
+"""
+    print("retrieve followed...")
+    more_users = retrieve_followed("CarlosRangelM78","")
 
+    while more_users != "End":
+        now=datetime.now()
+        print(f'Pause to wait for twitter license {now.strftime("%H:%M:%S")}')
+        time.sleep(60)
+        now = datetime.now()
+        print(f'Continuing retrieving  {now.strftime("%H:%M:%S")}')
+        print(f"Continue with token {more_users}")
+        more_users = retrieve_followed("CarlosRangelM78", more_users)
